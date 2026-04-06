d1183dfffe24394004ae5c88931f3e33abd1d032
Sarah06102 <sarah.hra06@gmail.com>
2026-04-06 19:27:06 -0400

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "API is working" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
